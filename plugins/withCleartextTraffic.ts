852a889ecf5b46ad3e44a27440e44a6aac265267
cn <cn@example.com>
2025-02-25 18:31:13 +0800

    changes

--- plugins/withCleartextTraffic.ts
+++ plugins/withCleartextTraffic.ts
@@ -1,12 +1,11 @@
-const { ConfigPlugin, withAndroidManifest } = require('@expo/config-plugins');
+const { ConfigPlugin, withAndroidManifest } = require('expo/config-plugins');
 
 const withCleartextTraffic = (config) => {
   return withAndroidManifest(config, (config) => {
-    if (config.modResults) {
-      const manifest = config.modResults.manifest.application ? config.modResults.manifest.application[0] : undefined;
-      if (manifest) {
-        manifest['android:usesCleartextTraffic'] = 'true';
-      }
+    const app = config.modResults.manifest.application?.[0];
+    if (app) {
+      // 正确操作 XML 属性的方式
+      app.$["android:usesCleartextTraffic"] = "true";
     }
     return config;
   });
